1945891bee858b038a581533a36b47d606804dbb
dariuszryn <dariusz.ryngwelski@hotmail.com>
2020-08-22 15:10:43 +0200

    Add json -> class Java

---
+++ src/main/java/pl/sdacademy/jsonClasses/Country.java
@@ -0,0 +1,133 @@
+package pl.sdacademy.jsonClasses;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Country {
+    private String country;
+    private String countryCode;
+    private String slug;
+    private int newConfirmed;
+    private int totalConfirmed;
+    private int newDeaths;
+    private int totalDeaths;
+    private int newRecovered;
+    private int totalRecovered;
+    private LocalDateTime date;
+    private final Set<String> premium = new HashSet<>();
+
+    public Country(String country, String countryCode, String slug, int newConfirmed, int totalConfirmed, int newDeaths, int totalDeaths, int newRecovered, int totalRecovered, LocalDateTime date) {
+        this.country = country;
+        this.countryCode = countryCode;
+        this.slug = slug;
+        this.newConfirmed = newConfirmed;
+        this.totalConfirmed = totalConfirmed;
+        this.newDeaths = newDeaths;
+        this.totalDeaths = totalDeaths;
+        this.newRecovered = newRecovered;
+        this.totalRecovered = totalRecovered;
+        this.date = date;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public int getNewConfirmed() {
+        return newConfirmed;
+    }
+
+    public void setNewConfirmed(int newConfirmed) {
+        this.newConfirmed = newConfirmed;
+    }
+
+    public int getTotalConfirmed() {
+        return totalConfirmed;
+    }
+
+    public void setTotalConfirmed(int totalConfirmed) {
+        this.totalConfirmed = totalConfirmed;
+    }
+
+    public int getNewDeaths() {
+        return newDeaths;
+    }
+
+    public void setNewDeaths(int newDeaths) {
+        this.newDeaths = newDeaths;
+    }
+
+    public int getTotalDeaths() {
+        return totalDeaths;
+    }
+
+    public void setTotalDeaths(int totalDeaths) {
+        this.totalDeaths = totalDeaths;
+    }
+
+    public int getNewRecovered() {
+        return newRecovered;
+    }
+
+    public void setNewRecovered(int newRecovered) {
+        this.newRecovered = newRecovered;
+    }
+
+    public int getTotalRecovered() {
+        return totalRecovered;
+    }
+
+    public void setTotalRecovered(int totalRecovered) {
+        this.totalRecovered = totalRecovered;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public Set<String> getPremium() {
+        return premium;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "country='" + country + '\'' +
+                ", countryCode='" + countryCode + '\'' +
+                ", slug='" + slug + '\'' +
+                ", newConfirmed=" + newConfirmed +
+                ", totalConfirmed=" + totalConfirmed +
+                ", newDeaths=" + newDeaths +
+                ", totalDeaths=" + totalDeaths +
+                ", newRecovered=" + newRecovered +
+                ", totalRecovered=" + totalRecovered +
+                ", date=" + date +
+                ", premium=" + premium +
+                '}';
+    }
+}
