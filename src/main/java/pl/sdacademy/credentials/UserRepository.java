1f3a59049f97b38ee66cef628a3ecd9b16f871ae
Bartek <bartlomiejgawron@wp.pl>
2020-08-23 14:56:08 +0200

    D3 changes in isContinuity method in the package of pl.sdacademy.utils;

---
+++ src/main/java/pl/sdacademy/credentials/UserRepository.java
@@ -0,0 +1,21 @@
+package pl.sdacademy.credentials;
+
+
+public interface UserRepository {
+
+    //Utwórz interfejs UserRepository z metodami CRUD (readById, readAll, create, update, delete)
+
+
+    void readById();
+
+    void readAll();
+
+    void create();
+
+    void update();
+
+    void delete();
+
+
+}
+
