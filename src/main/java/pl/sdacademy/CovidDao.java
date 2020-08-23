f15145f367544155602b3e01684551c21022b9c6
przemekp <przemyslaw.piwonski@gmail.com>
2020-08-23 10:37:05 +0200

    Created interface CovidDao with methods (without implementations)

---
+++ src/main/java/pl/sdacademy/CovidDao.java
@@ -0,0 +1,26 @@
+package pl.sdacademy;
+
+//Utwórz interfejs CovidDao. Oferuje metody (abstrakcyjne - bez implementacji):
+//        - getCountries, która zwróci listę wszystkich krajów (bez danych dot. wirusa)
+//        - getDataByCountryAndDateRange, której argumentami są identyfikator kraju oraz dwie daty - zakres od i do
+//        - getCurrentDataByCountry, której argumentem jest identyfikator kraju - zwróci dane z dnia dzisiejszego
+//        - getCurrentWorldData, która zwróci zagregowane, dzisiejsze dane z całego świata
+//        - storeData, która usunie wszystkie dane, a następnie utrwali listę krajów, którą otrzyma jako parametr.
+
+import pl.sdacademy.entities.Country;
+import pl.sdacademy.entities.StoreData;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface CovidDao {
+    public abstract List<Country> getCountries();
+
+    public StoreData getDataByCountryAndDateRange(int id, LocalDate from, LocalDate to);
+
+    public StoreData getCurrentDataByCountry(int id);
+
+    public StoreData getCurrentWorldData();
+
+    public void storeData(List<Country> countryList);
+}
