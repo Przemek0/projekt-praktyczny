c6e660f36a0a6d96d6f3f2767ab18f265f740580
Bartek <bartlomiejgawron@wp.pl>
2020-08-23 13:27:44 +0200

    C1 Added pl.sdacademy.credentials.User class;

---
+++ src/main/java/pl/sdacademy/credentials/User.java
@@ -0,0 +1,14 @@
+package pl.sdacademy.credentials;
+
+import java.time.LocalDate;
+
+public class User {
+
+    //C1 Utwórz klasę User w pakiecie pl.sdacademy.credentials, o polach: id, firstName, lastName, dateOfBirth, admin (boolean)
+    int id;
+    String firstName;
+    String lastName;
+    LocalDate dateOfBirth;
+    boolean admin;
+
+}
