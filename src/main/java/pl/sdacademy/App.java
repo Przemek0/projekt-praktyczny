fa97157a5d4545d83a83031ecdfa8597fa6c2986
Karola <karolinatrzaska@wp.pl>
2020-08-23 10:18:10 +0200

    Ex FX2 Utworzono widok main.fxml. zmieniono w App na"main"

--- src/main/java/pl/sdacademy/App.java
+++ src/main/java/pl/sdacademy/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
+        scene = new Scene(loadFXML("main"));
         stage.setScene(scene);
         stage.show();
     }
