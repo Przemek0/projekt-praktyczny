61be1d09619adafedf071d4217f60beb6a7185f6
Bartek <bartlomiejgawron@wp.pl>
2020-08-23 13:09:50 +0200

    D3 Added method isContinuity to pl.sdacademy.utils.DateUtils.java

--- src/main/java/pl/sdacademy/utils/DateUtils.java
+++ src/main/java/pl/sdacademy/utils/DateUtils.java
@@ -1,13 +1,30 @@
 package pl.sdacademy.utils;
 
 import java.time.LocalDate;
+import java.util.TreeSet;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 
 public class DateUtils {
-    //Dodaj do klasy DateUtils metodę, która przyjmie dwa parametry - dwie daty (obiekty typu LocalDate), a która zwróci liczbę dni dzielących te daty.
-    public int numberOfDaysBetween(LocalDate fromDate, LocalDate toDate){
+
+
+    //Dodaj do klasy DateUtils metodę, która przyjmie dwa parametry
+    //- dwie daty (obiekty typu LocalDate), a która zwróci liczbę dni dzielących te daty.
+    public static int numberOfDaysBetween(LocalDate fromDate, LocalDate toDate) {
         int noOfDaysBetween = (int) DAYS.between(fromDate, toDate);
         return noOfDaysBetween;
+    }
+
+    //Dodaj do klasy DateUtils metodę, która przyjmie parametr
+    // - zbiór dat, a która zwróci odpowiedź, czy między datami ze zbioru jest jakakolwiek luka.
+    public static boolean isContinuity(TreeSet<LocalDate> set) {
+        LocalDate first = set.first();
+        LocalDate last = set.last();
+        System.out.println("size " + set.size());
+        int daysBetweenFirstAndLast = (int) DAYS.between(first, last);
+        System.out.println("days between " + (daysBetweenFirstAndLast + 1));
+        if (daysBetweenFirstAndLast + 1 == set.size())
+            return true;
+        else return false;
     }
 }
