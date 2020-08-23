9e2af42d2772e78e5b429eca495bc9b2ee8b3c20
Karola <karolinatrzaska@wp.pl>
2020-08-23 14:01:54 +0200

    Ex C9 - class User changed to entities

--- src/main/java/pl/sdacademy/credentials/User.java
+++ src/main/java/pl/sdacademy/credentials/User.java
@@ -1,8 +1,16 @@
 package pl.sdacademy.credentials;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
 
     //C1 Utwórz klasę User w pakiecie pl.sdacademy.credentials, o polach: id, firstName, lastName, dateOfBirth, admin (boolean)
     int id;
