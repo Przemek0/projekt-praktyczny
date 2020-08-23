0a4573e433eba7b0786d31a6e87845b97cd565d2
przemekp <przemyslaw.piwonski@gmail.com>
2020-08-23 15:01:05 +0200

    FX19, FX3 Added openInNewWindow method which opens views in new window. Added action to showChartBtn which opens dataChart in new window Added showChartBtn id to "Wyświetl wykres"

--- src/main/java/pl/sdacademy/PrimaryController.java
+++ src/main/java/pl/sdacademy/PrimaryController.java
@@ -1,7 +1,35 @@
 package pl.sdacademy;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
 public class PrimaryController {
+    @FXML
+    private Button showChartBtn;
 
+    public void initialize() {
+        showChartBtn.setOnAction(event -> {
+            openInNewWindow("dataChart.fxml");
+        });
+    }
 
-    public void initialize(){}
+    private void openInNewWindow(String fxml) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource(fxml));
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
