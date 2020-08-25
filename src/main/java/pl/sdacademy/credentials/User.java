21dd629e066452e9fb0059e9bba0fc39f6846a63
przemekp <przemyslaw.piwonski@gmail.com>
2020-08-25 07:26:48 +0200

    C1 wprowadzono zmiany, aby klasa była zgodna z treścią zadania

--- src/main/java/pl/sdacademy/credentials/User.java
+++ src/main/java/pl/sdacademy/credentials/User.java
@@ -1,72 +1,12 @@
 package pl.sdacademy.credentials;
 
-import pl.sdacademy.entities.AbstractEntity;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.time.LocalDate;
 
-@Entity
-public class User extends AbstractEntity {
-
+public class User {
     //C1 Utwórz klasę User w pakiecie pl.sdacademy.credentials, o polach: id, firstName, lastName, dateOfBirth, admin (boolean)
-    String firstName;
-    String lastName;
-    LocalDate dateOfBirth;
-    boolean admin;
-
-    public User() {
-    }
-
-    public User(String firstName, String lastName, LocalDate dateOfBirth, boolean admin) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.dateOfBirth = dateOfBirth;
-        this.admin = admin;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public boolean isAdmin() {
-        return admin;
-    }
-
-    public void setAdmin(boolean admin) {
-        this.admin = admin;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", dateOfBirth=" + dateOfBirth +
-                ", admin=" + admin +
-                '}';
-    }
+    private int id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private boolean admin;
 }
