5c8a36603848bf96664aecee77a7c471810dc907
przemekp <przemyslaw.piwonski@gmail.com>
2020-08-26 12:24:38 +0200

    C11 Dodano implementacje metody readAll w klasie HibernateUserRepository

--- src/main/java/pl/sdacademy/credentials/HibernateUserRepository.java
+++ src/main/java/pl/sdacademy/credentials/HibernateUserRepository.java
@@ -1,14 +1,26 @@
 package pl.sdacademy.credentials;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 public class HibernateUserRepository implements UserRepository{
+    private SessionFactory sessionFactory = new Configuration()
+            .configure()
+            .buildSessionFactory();
+
     @Override
     public void readById(int id) {
-
     }
 
     @Override
-    public void readAll() {
-
+    public List<User> readAll() {
+        Session session = sessionFactory.openSession();
+        Query<User> readAllUsers = session.createQuery("SELECT u FROM User u", User.class);
+        return readAllUsers.getResultList();
     }
 
     @Override
